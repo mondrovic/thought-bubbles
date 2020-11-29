c673ccd9aeef25dce8ca46591a3fc275f1bd5b7d
Matthew O <mo@pop-os.localdomain>
2020-11-28 20:37:53 -0700

    setup user model and server

---
+++ models/User.js
@@ -0,0 +1,50 @@
+// Need to add validator to email
+
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// virtual to count users
+
+/*
+schema.virtual runs get
+  returns thoughts with reduce
+    reduces (total, comment) into total + thoughts.length +1
+*/
+
+const User = model("User", userSchema);
+
+module.exports = User;
